fcc0b582516e025b2367eddda20b5569133388ad
宋慧武 <songhuiwu001@ke.com>
2019-07-23 15:40:48 +0800

    :sparkles:better support async track

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * Created Date: 2019-07-08
  * Author: 宋慧武
  * ------
- * Last Modified: Wednesday 2019-07-17 11:33:12 am
+ * Last Modified: Tuesday 2019-07-23 14:43:56 pm
  * Modified By: the developer formerly known as 宋慧武 at <songhuiwu001@ke.com>
  * ------
  * HISTORY:
@@ -10,13 +10,8 @@
  * Javascript will save your soul!
  */
 import { isVisible } from "./utils/dom";
-import {
-  zipObject,
-  vaildEvent,
-  vaildWatchKey,
-  vaildReactComponent,
-  clearTimeoutQueue
-} from "./utils/helper";
+import { zipObject, clearTimeoutQueue } from "./utils/helper";
+import { vaildEvent, vaildWatchKey, vaildRC } from "./utils/error";
 
 const ONCE = "once";
 const modifiers = {
@@ -51,6 +46,40 @@ export const inject = (...props) => target => {
 export function track(modifier, eventId, params = {}) {
   const [mdfs] = zipObject(modifiers);
 
+  // 获取模块对应的埋点的适配器
+  function getTrackEvents(ctx) {
+    return ctx.trackEvents || ctx.props.trackEvents;
+  }
+
+  // 当前实例是否为react class组件
+  function checkRC(ctx) {
+    return ctx.isReactComponent;
+  }
+
+  // 生成唯一的once标记属性
+  function createOnceProp(name, eventId) {
+    return `${name}_${eventId}_${ONCE}`;
+  }
+
+  // 描述符value适配器
+  function valueAdapter(ctx, value, initializer, args) {
+    if (value) {
+      return value.bind(ctx);
+    }
+    if (initializer) {
+      return initializer.apply(ctx, args);
+    }
+  }
+
+  // 生成不同队列，即value执行前的值
+  function queueAdapter(ctx, fn, tck, ops) {
+    if (ops.after) {
+      return [fn, ops.isRC ? () => ctx.setState({}, tck) : tck];
+    } else {
+      return [tck, fn];
+    }
+  }
+
   if (!mdfs.includes(modifier.replace(/\.once/g, ""))) {
     throw new Error(`modifier '${modifier}' does not exist`);
   }
@@ -60,12 +89,13 @@ export function track(modifier, eventId, params = {}) {
     // 页面初始化埋点
     if (modifier.includes(modifiers.UVPV)) {
       handler = function(...args) {
-        vaildReactComponent(this.isReactComponent);
+        vaildRC(this.isReactComponent);
 
-        const evts = this.trackEvents || this.props.trackEvents;
-        const fn = value ? value.bind(this) : initializer.apply(this, args);
+        const evts = getTrackEvents(this);
+        const fn = valueAdapter(this, value, initializer, args);
         const tck = () => {
           const context = { ...this.state, ...this.props };
+
           evts[modifier].call(null, context, ...args);
         };
 
@@ -75,8 +105,8 @@ export function track(modifier, eventId, params = {}) {
     // 设置进入页面时间
     else if (modifier.includes(modifiers.TONP)) {
       handler = function(...args) {
-        const isRC = vaildReactComponent(this.isReactComponent); // 是否为 react 组件
-        const evts = this.trackEvents || this.props.trackEvents;
+        const isRC = vaildRC(this.isReactComponent); // 是否为 react 组件
+        const evts = getTrackEvents(this);
         const tck = () => {
           const stt = Date.now() - this.__trackPageEntryTime;
           const context = { ...this.state, ...this.props };
@@ -106,39 +136,91 @@ export function track(modifier, eventId, params = {}) {
           : initializer.apply(this, args)(...args);
       };
     }
-    // 事件行为埋点
-    else if (modifier.includes(modifiers.TRIGGER)) {
+    // 事件行为埋点(支持同步、异步)
+    else if (
+      (modifier.includes(modifiers.TRIGGER) ||
+        modifier.includes(modifiers.ASYNC)) &&
+      (!params.stateKey && !params.propKey)
+    ) {
       handler = function(...args) {
+        let tck;
         let context = this;
-        const isRC = this.isReactComponent; // 是否为 react 组件
-        const evts = this.trackEvents || this.props.trackEvents;
-        const once = modifier.includes(ONCE);
-        const onceProp = `${name}_${eventId}`;
-        const after = modifier.includes(modifiers.TRIGGER_AFTER);
-        const tck = () => {
+        const isRC = checkRC(this); // 是否为 react 组件
+        const evts = getTrackEvents(this);
+        const onceProp = createOnceProp(name, eventId);
+        const fn = valueAdapter(this, value, initializer, args);
+        const after =
+          modifier.includes(modifiers.TRIGGER_AFTER) ||
+          modifier.includes(modifiers.ASYNC);
+
+        tck = () => {
           if (this[onceProp]) return; // 如果存在once修饰符，且为true则直接返回
           vaildEvent(evts, eventId); // 检测eventId是否合法
           isRC && (context = { ...this.state, ...this.props });
           evts[eventId].call(null, context, ...args);
-          once && (this[onceProp] = true);
+          modifier.includes(ONCE) && (this[onceProp] = true);
         };
-        const fn = value ? value.bind(this) : initializer.apply(this, args);
-        const queue = after
-          ? [fn, isRC ? () => this.setState({}, tck) : tck]
-          : [tck, fn];
 
-        return queue.forEach(sub => sub(...args));
+        if (
+          modifier === modifiers.ASYNC_DELAY ||
+          modifier === modifiers.ASYNC_DELAY + ".once"
+        ) {
+          !this.tckTimerQueue && (this.tckTimerQueue = {}); // 定时器队列
+          tck = () => {
+            const { delay = 0, ref } = params;
+            const ele = this[ref] || document;
+            const timer = this.tckTimerQueue[eventId];
+
+            timer && clearTimeout(timer);
+            this.tckTimerQueue[eventId] = setTimeout(() => {
+              isRC && (context = { ...this.state, ...this.props });
+              isVisible(ele) && evts[eventId].call(null, context, ...args);
+              clearTimeout(this.tckTimerQueue[eventId]);
+            }, delay);
+          };
+
+          // 页面卸载时清除进行中的定时器
+          if (isRC && !this.componentWillUnmount) {
+            this.needMergeWillUnmount = false;
+            this.componentWillUnmount = () => {
+              clearTimeoutQueue(this.tckTimerQueue);
+            };
+          } else if (
+            this.componentWillUnmount &&
+            this.needMergeWillUnmount !== false
+          ) {
+            const willUnmountRef = this.componentWillUnmount;
+            this.componentWillUnmount = () => {
+              willUnmountRef.apply(this);
+              clearTimeoutQueue(this.tckTimerQueue);
+            };
+          }
+        }
+
+        const queue = queueAdapter(this, fn, tck, { isRC, after });
+
+        if (modifier.includes(modifiers.ASYNC)) {
+          // 异步
+          return (async () => {
+            for (const iter of queue) {
+              await iter(...args);
+            }
+          })();
+        } else {
+          // 同步
+          return queue.forEach(sub => sub(...args));
+        }
       };
     }
-    // 异步行为埋点
+    // 异步监听行为埋点
     else {
       handler = function(...args) {
         let tck;
         let context = this;
-        const isRC = this.isReactComponent; // 是否为 react 组件
-        const evts = this.trackEvents || this.props.trackEvents;
+        const isRC = checkRC(this); // 是否为 react 组件
+        const evts = getTrackEvents(this);
+        const fn = valueAdapter(this, value, initializer, args);
         const once = modifier.includes(ONCE);
-        const fn = value ? value.bind(this) : initializer.apply(this, args);
         const { stateKey, propKey } = params;
         const watchPropKey = `${name}_${propKey}_${eventId}`; // 保证key的唯一性
         const watchStateKey = `${name}_${stateKey}_${eventId}`;
