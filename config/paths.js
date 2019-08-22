b90e0b99603dd57bb10b3bc894af4e108b17e456
宋慧武 <songhuiwu001@ke.com>
2019-08-22 18:37:34 +0800

    [Chore]build docs

--- config/paths.js
+++ config/paths.js
@@ -67,7 +67,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp(".env"),
   appPath: resolveApp("."),
-  appBuild: resolveApp("build"),
+  appBuild: resolveApp("docs/dist"),
   appPublic: resolveApp("public"),
   appHtml: resolveApp("public/index.html"),
   appIndexJs: resolveModule(resolveApp, "docs/index"),
---
+++ docs/dist/precache-manifest.42a7a6ecc09100b40d1acd2e4fd32e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f1c792509ffa292902ecacb85064aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfc57ae71d96f1007d85",
+    "url": "/static/css/2.bc3eb4e0.chunk.css"
+  },
+  {
+    "revision": "74bcce2dd8b9847bb2e8",
+    "url": "/static/css/main.fb47905e.chunk.css"
+  },
+  {
+    "revision": "dfc57ae71d96f1007d85",
+    "url": "/static/js/2.881ea1fa.chunk.js"
+  },
+  {
+    "revision": "74bcce2dd8b9847bb2e8",
+    "url": "/static/js/main.816df254.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
